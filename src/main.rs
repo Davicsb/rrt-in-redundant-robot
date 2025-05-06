76bb0d1e4907cab2ce38e6218818e7a0710452d9
Davi Celestino <davi.csb@gmail.com>
2025-05-06 10:35:11 -0300

    Primeiros códigos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main (){
+    println!("Guess the number!");
+    println!("Guess a number.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Error readind a line");
+
+    println!("Your guess: {}", guess);
 }
