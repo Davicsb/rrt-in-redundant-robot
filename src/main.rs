dad867d9d0c16966ec50518b167c617471c690d1
Davi Celestino <davi.csb@gmail.com>
2025-06-12 14:01:04 -0300

    Chapter 4

--- src/main.rs
+++ src/main.rs
@@ -3,38 +3,34 @@ use std::io;
 fn read_number() -> i32{
     let mut num = String::new();
     io::stdin().read_line(&mut num).expect("Fail!");
-    let num: i32 = num.trim().parse(). expect("Invalid Number");
+    let num: i32 = num.trim().parse().expect("Invalid Number");
     num
 }
 
-fn sum(x: i32, y: i32) -> i32{
-    x + y
+fn read_string() -> String{
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).expect("Fail!");
+    s
 }
 
-fn sub(x: i32, y: i32) -> i32{
-    x - y
+fn len_calculator(s : &String) -> usize{
+    s.trim().len()
 }
 
 fn main (){
-    println!("==Calculator==");
+    println!("==String len calculator==");
     loop{
         println!("What you wanna do?");
-        println!("0 - Exit\n1 - Sum\n2 - Sub");
+        println!("0 - Exit\n1 - Read string");
         let option : i32 = read_number();
         if option == 0{
             break;
         }
         else if option == 1{
-            println!("Enter the two numbers.");
-            let x = read_number();
-            let y = read_number();
-            println!("Your Sum = {}.", sum(x, y));
-        }
-        else if option == 2{
-            println!("Enter the two numbers.");
-            let x = read_number();
-            let y = read_number();
-            println!("Your Sub = {}.", sub(x, y));
+            println!("Enter the string.");
+            let s = read_string();
+            let len = len_calculator(&s);
+            println!("Len = {}.", len);
         }
         else{
             println!("Press a valid option!")
