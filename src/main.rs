51b6d5884708529a4d20537bc57fc062ef09bc32
Davi Celestino <davi.csb@gmail.com>
2025-06-18 10:56:32 -0300

    Cap.5

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
 use std::io;
+use std::fmt;
+
+struct Color(i32, i32, i32);
+
+impl fmt::Display for Color {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "rgb:({}, {}, {})", self.0, self.1, self.2)
+    }
+}
 
 fn read_number() -> i32{
     let mut num = String::new();
@@ -17,11 +26,21 @@ fn len_calculator(s : &String) -> usize{
     s.trim().len()
 }
 
+fn first_slice(s : &str) -> &str{
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate(){
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    &s[..]
+}
+
 fn main (){
     println!("==String len calculator==");
     loop{
         println!("What you wanna do?");
-        println!("0 - Exit\n1 - Read string");
+        println!("0 - Exit\n1 - Len string calculator\n2 - First slice\n3 - View collors");
         let option : i32 = read_number();
         if option == 0{
             break;
@@ -31,6 +50,36 @@ fn main (){
             let s = read_string();
             let len = len_calculator(&s);
             println!("Len = {}.", len);
+        }
+        else if option == 2{
+            println!("Enter the string.");
+            let s = read_string();
+            let word = first_slice(&s);
+            println!("First slice: {}.", word);
+        }
+        else if option == 3{
+            let black = Color(0, 0, 0);
+            let red = Color(255, 0, 0);
+            let white = Color(255, 255, 255);
+            loop{
+                println!("0 - Exit\n1 - Black\n2 - Red\n3 - White");
+                let choose : i32 = read_number();
+                if choose == 0{
+                    break;
+                }
+                if choose == 1{
+                    println!("{black}");
+                }
+                else if choose == 2{
+                    println!("{red}");
+                }
+                else if choose == 3{
+                    println!("{white}");
+                }
+                else{
+                    println!("Invalid option, try again!")
+                } 
+            }
         }
         else{
             println!("Press a valid option!")
