bc2964cb9454a4177cf5f69d2fd2263e0e0a80fb
Davi Celestino <davi.csb@gmail.com>
2025-07-15 10:50:33 -0300

    Chapter 5.2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 use std::io;
-use std::fmt;
 
-struct Color(i32, i32, i32);
-
-impl fmt::Display for Color {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "rgb:({}, {}, {})", self.0, self.1, self.2)
-    }
+struct Triangulo{
+    base: i32,
+    altura: i32,
 }
 
 fn read_number() -> i32{
@@ -16,74 +12,18 @@ fn read_number() -> i32{
     num
 }
 
-fn read_string() -> String{
-    let mut s = String::new();
-    io::stdin().read_line(&mut s).expect("Fail!");
-    s
+fn cal_area(triangulo: &Triangulo) -> i32{
+    (triangulo.base * triangulo.altura)/2
 }
 
-fn len_calculator(s : &String) -> usize{
-    s.trim().len()
-}
-
-fn first_slice(s : &str) -> &str{
-    let bytes = s.as_bytes();
-    for (i, &item) in bytes.iter().enumerate(){
-        if item == b' ' {
-            return &s[..i];
-        }
-    }
-    &s[..]
-}
-
-fn main (){
-    println!("==String len calculator==");
-    loop{
-        println!("What you wanna do?");
-        println!("0 - Exit\n1 - Len string calculator\n2 - First slice\n3 - View collors");
-        let option : i32 = read_number();
-        if option == 0{
-            break;
-        }
-        else if option == 1{
-            println!("Enter the string.");
-            let s = read_string();
-            let len = len_calculator(&s);
-            println!("Len = {}.", len);
-        }
-        else if option == 2{
-            println!("Enter the string.");
-            let s = read_string();
-            let word = first_slice(&s);
-            println!("First slice: {}.", word);
-        }
-        else if option == 3{
-            let black = Color(0, 0, 0);
-            let red = Color(255, 0, 0);
-            let white = Color(255, 255, 255);
-            loop{
-                println!("0 - Exit\n1 - Black\n2 - Red\n3 - White");
-                let choose : i32 = read_number();
-                if choose == 0{
-                    break;
-                }
-                if choose == 1{
-                    println!("{black}");
-                }
-                else if choose == 2{
-                    println!("{red}");
-                }
-                else if choose == 3{
-                    println!("{white}");
-                }
-                else{
-                    println!("Invalid option, try again!")
-                } 
-            }
-        }
-        else{
-            println!("Press a valid option!")
-        }
-    }
-    
+fn main(){
+    println!("Calcule a área do seu triângulo.\nDigite a base:");
+    let base : i32 = read_number();
+    println!("Altura:");
+    let altura : i32 = read_number();
+    let figura = Triangulo{
+        base,
+        altura,
+    };
+    println!("A área do seu triângulo é: {}", cal_area(&figura));
 }
